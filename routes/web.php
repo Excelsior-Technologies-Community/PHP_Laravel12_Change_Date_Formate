853fd71e9aabcdb8e088d3b06d862446e2577b83
YashPatel4268 <142138437+YashPatel4268@users.noreply.github.com>
2026-09-14 13:33:11 +0530

    feat: add date difference calculator, human-readable date conversion, and timezone conversion tools

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\DemoController;
 
+// Existing date format examples
 Route::get('/date1', [DemoController::class, 'index']);
 Route::get('/example2', [DemoController::class, 'index']);
 Route::get('/example3', [DemoController::class, 'index']);
 Route::get('/example4', [DemoController::class, 'index']);
 Route::get('/example5', [DemoController::class, 'index']);
+
+// Date Difference Calculator
+Route::get('/date-difference', [DemoController::class, 'dateDifference']);
+Route::post('/date-difference', [DemoController::class, 'calculateDateDifference'])
+    ->name('date.difference');
+
+// Human-readable Date Converter
+Route::get('/human-readable-date', [DemoController::class, 'humanReadable']);
+Route::post('/human-readable-date', [DemoController::class, 'convertToHumanReadable'])
+    ->name('human.readable');
+
+// Timezone Date Converter
+Route::get('/timezone-converter', [DemoController::class, 'timezoneConverter']);
+Route::post('/timezone-converter', [DemoController::class, 'convertTimezone'])
+    ->name('timezone.convert');
